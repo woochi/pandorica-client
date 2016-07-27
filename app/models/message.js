4a43568fc7a05775a03bf29bf836dc70a1fd8a0a
Mikko Kivelä <mikko@smartly.io>
2016-07-27 19:00:18 +0300

    Remove redundant import

--- app/models/message.js
+++ app/models/message.js
@@ -1,12 +1,7 @@
 import {Schema} from 'normalizr';
-import Faction from './Faction';
 
 const Message = new Schema('messages', {
   idAttribute: '_id'
-});
-
-Message.define({
-  faction: Faction
 });
 
 export default Message;
